c751dde5ce6e7ec9bede50679d7681ab41cdf1d1
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-22 14:44:59 +0100

    aggiunge somma e differenza

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 public static class CalcoliHelper
 {
+    public static int Somma(int a, int b) => a + b;
 
+    public static double SommaDouble(double a, double b) => a + b;
+
+    public static int Differenza(int a, int b) => a - b;
+
+    public static double DifferenzaDouble(double a, double b) => a - b;
 }
 
